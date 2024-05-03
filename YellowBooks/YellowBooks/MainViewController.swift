041383e4b75a16ab097599585c5786da02ae3605
HyeonJeong <dlaus1214@gmail.com>
2024-05-03 19:42:26 +0900

    MainVC UI done

--- YellowBooks/YellowBooks/MainViewController.swift
+++ YellowBooks/YellowBooks/MainViewController.swift
@@ -6,15 +6,121 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController {
 
+    
+
+    let brandLogoImage = UIImageView(image: UIImage(named: "brandLogo"))
+    let searchBarView = UIView()
+    let searchImage = UIImageView(image: UIImage(systemName: "magnifyingglass"))
+//    let searchBarButton = UIButton()
+    let allListLabel = UILabel()
+    let allBookListColletionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = .init(top: 0, left: 16, bottom: 0, right: 16)
+        
+        layout.minimumLineSpacing = 8
+        layout.minimumInteritemSpacing = 8
+        let spacing: CGFloat = 8 // 내가 설정한 간격(minimumLineSpacing)이 8이므로
+        let deviceWidth = UIScreen.main.bounds.width
+        let countForLine: CGFloat = 3 // 한줄에 3개 넣을거니까
+        let inset: CGFloat = 16 // 추가로 적용한 가로 inset
+        
+        let cellWidth = (deviceWidth - spacing * (countForLine - 1) - inset * 2 - 1)/countForLine
+        // -1을 해준 이유는 float의 경우 나눗셈에서 0.33이 되지만 이게 3개가 모였을 때 부동소숫점 문제가 발생해서 근사치로 계산했습니다.
+        // floor 를 통해 내림처리 해주는 방법도 있습니다!!
+        
+        layout.itemSize = .init(width: cellWidth, height: cellWidth * 1.5)
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        return cv
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        // Do any additional setup after loading the view.
+        
+        allBookListColletionView.dataSource = self
+        allBookListColletionView.delegate = self
+        setupConstraints()
+        configureUI()
     }
-
+    
+    
+    func setupConstraints() {
+        view.addSubview(brandLogoImage)
+        view.addSubview(searchBarView)
+        searchBarView.addSubview(searchImage)
+        view.addSubview(allListLabel)
+        view.addSubview(allBookListColletionView)
+        
+        brandLogoImage.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(50)
+            $0.leading.equalToSuperview().offset(16)
+            $0.height.width.equalTo(30)
+        }
+        
+        searchBarView.snp.makeConstraints {
+            $0.top.equalTo(brandLogoImage.snp.bottom).offset(10)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().offset(-16)
+            $0.height.equalTo(50)
+        }
+        
+        searchImage.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().offset(10)
+            $0.bottom.equalToSuperview().offset(-10)
+            $0.height.width.equalTo(30)
+        }
+        
+        allListLabel.snp.makeConstraints {
+            $0.top.equalTo(searchBarView.snp.bottom).offset(10)
+            $0.leading.equalToSuperview().offset(16)
+        }
+        
+        allBookListColletionView.snp.makeConstraints {
+            $0.top.equalTo(allListLabel.snp.bottom).offset(10)
+            $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    func configureUI() {
+        searchBarView.backgroundColor = .systemBackground
+        searchBarView.layer.cornerRadius = 7
+        searchBarView.layer.borderWidth = 2
+        searchBarView.layer.borderColor = UIColor.gray.cgColor
+        searchBarView.tintColor = .ybgray
+        
+        searchBarView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(goToSearchView(_:))))
+        
+        allListLabel.text = "All Books"
+        allListLabel.textColor = .ybblack
+        allListLabel.font = .systemFont(ofSize: 20, weight: .heavy)
+        
+        allBookListColletionView.backgroundColor = .clear
+        allBookListColletionView.register(AllBookListCollectionViewCell.self, forCellWithReuseIdentifier: AllBookListCollectionViewCell.identifier)
+    }
+    
+    @objc func goToSearchView(_ gesture: UITapGestureRecognizer) {
+        print("clicked searchVIew")
+        
+    }
 
 }
 
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = allBookListColletionView.dequeueReusableCell(withReuseIdentifier: AllBookListCollectionViewCell.identifier, for: indexPath) as? AllBookListCollectionViewCell else { return AllBookListCollectionViewCell() }
+        
+//        cell.layer.cornerRadius = 6
+        return cell
+    }
+    
+}
--- YellowBooks/YellowBooks/SearchViewController.swift
+++ YellowBooks/YellowBooks/SearchViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    let searchBar = UISearchBar()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        // Do any additional setup after loading the view.
+        setupConstraints()
+        configureUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func setupConstraints() {
+        
     }
-    */
+    func configureUI() {
+        
+    }
 
 }
